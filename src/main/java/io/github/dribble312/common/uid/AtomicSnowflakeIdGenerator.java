f3f73020cd5ff0227b3eb2fa5fbdf265260eaa74
chase chen <906184146@qq.com>
2024-09-18 16:20:15 +0800

    set dribble-java-dependencies as parent (for the source and doc plugin)

--- src/main/java/io/github/dribble312/common/uid/AtomicSnowflakeIdGenerator.java
+++ src/main/java/io/github/dribble312/common/uid/AtomicSnowflakeIdGenerator.java
@@ -159,71 +159,71 @@ public class AtomicSnowflakeIdGenerator {
     }
 
 
-    // public static void main(String[] args) {
-    //     AtomicSnowflakeIdGenerator snowflakeIdGenerator = new AtomicSnowflakeIdGenerator(0, 0);
-    //     Map<Long, Integer> map = new ConcurrentHashMap<>();
-    //
-    //     ThreadPoolExecutor executorService = (ThreadPoolExecutor) Executors.newFixedThreadPool(20);
-    //
-    //     // 使用 ScheduledExecutorService 每隔一段时间打印线程池状态
-    //     ScheduledExecutorService monitorService = Executors.newScheduledThreadPool(1);
-    //
-    //
-    //     ArrayList<Callable<String>> tasks = new ArrayList<>();
-    //     for (int j = 0; j < 10; j++) {
-    //         int finalJ = j;
-    //         tasks.add(() -> {
-    //             final int taskId = finalJ;
-    //             long st = System.currentTimeMillis();
-    //             for (int i = 0; i < 7_000000; i++) {
-    //                 long id = snowflakeIdGenerator.nextId();
-    //                 if (map.containsKey(id)) {
-    //                     throw new RuntimeException("Duplicate id: " + Long.toBinaryString(id));
-    //                 }
-    //                 map.put(id, 0);
-    //             }
-    //             long ed = System.currentTimeMillis();
-    //             System.out.println("Cost time: " + (ed - st) + " map size " + map.size());
-    //             return "Task " + taskId + " completed.";
-    //         });
-    //     }
-    //
-    //     long start = System.currentTimeMillis();
-    //
-    //     // 定时任务：每隔 2 秒打印一次线程池状态
-    //     monitorService.scheduleAtFixedRate(() -> {
-    //         System.out.println("==== ThreadPool Status ====");
-    //         System.out.println("total: " + map.size());
-    //         System.out.println("Average generate " + map.size() / (System.currentTimeMillis() - start) + " id per ms");
-    //         System.out.println(
-    //                 "Average Spin time " + spin.get().divide(BigDecimal.valueOf(map.size()), 2, RoundingMode.HALF_UP));
-    //         System.out.println("Active Threads: " + executorService.getActiveCount());
-    //         System.out.println("Total Tasks: " + executorService.getTaskCount());
-    //         System.out.println("Completed Tasks: " + executorService.getCompletedTaskCount());
-    //         System.out.println("Pool Size: " + executorService.getPoolSize());
-    //         System.out.println("Queue Size: " + executorService.getQueue().size());
-    //         System.out.println("===========================");
-    //     }, 0, 2, TimeUnit.SECONDS);
-    //
-    //     try {
-    //         List<Future<String>> futures = executorService.invokeAll(tasks);
-    //
-    //         for (Future<String> future : futures) {
-    //             System.out.println(future.get());
-    //         }
-    //
-    //         long end = System.currentTimeMillis();
-    //
-    //         System.out.println("Out Cost time: " + (end - start) + "ms");
-    //         System.out.println("Out Cost size: " + map.size());
-    //         System.out.println("Average generate " + map.size() / (end - start) + " id per ms");
-    //         System.out.println("Average Spin time for each id: " + spin.get()
-    //                 .divide(BigDecimal.valueOf(map.size()), 2, RoundingMode.HALF_UP));
-    //     } catch (InterruptedException | ExecutionException e) {
-    //         throw new RuntimeException(e);
-    //     } finally {
-    //         executorService.close();
-    //     }
-    // }
+    public static void main(String[] args) {
+        AtomicSnowflakeIdGenerator snowflakeIdGenerator = new AtomicSnowflakeIdGenerator(0, 0);
+        Map<Long, Integer> map = new ConcurrentHashMap<>();
+
+        ThreadPoolExecutor executorService = (ThreadPoolExecutor) Executors.newFixedThreadPool(20);
+
+        // 使用 ScheduledExecutorService 每隔一段时间打印线程池状态
+        ScheduledExecutorService monitorService = Executors.newScheduledThreadPool(1);
+
+
+        ArrayList<Callable<String>> tasks = new ArrayList<>();
+        for (int j = 0; j < 10; j++) {
+            int finalJ = j;
+            tasks.add(() -> {
+                final int taskId = finalJ;
+                long st = System.currentTimeMillis();
+                for (int i = 0; i < 7_000000; i++) {
+                    long id = snowflakeIdGenerator.nextId();
+                    if (map.containsKey(id)) {
+                        throw new RuntimeException("Duplicate id: " + Long.toBinaryString(id));
+                    }
+                    map.put(id, 0);
+                }
+                long ed = System.currentTimeMillis();
+                System.out.println("Cost time: " + (ed - st) + " map size " + map.size());
+                return "Task " + taskId + " completed.";
+            });
+        }
+
+        long start = System.currentTimeMillis();
+
+        // 定时任务：每隔 2 秒打印一次线程池状态
+        monitorService.scheduleAtFixedRate(() -> {
+            System.out.println("==== ThreadPool Status ====");
+            System.out.println("total: " + map.size());
+            System.out.println("Average generate " + map.size() / (System.currentTimeMillis() - start) + " id per ms");
+            System.out.println(
+                    "Average Spin time " + spin.get().divide(BigDecimal.valueOf(map.size()), 2, RoundingMode.HALF_UP));
+            System.out.println("Active Threads: " + executorService.getActiveCount());
+            System.out.println("Total Tasks: " + executorService.getTaskCount());
+            System.out.println("Completed Tasks: " + executorService.getCompletedTaskCount());
+            System.out.println("Pool Size: " + executorService.getPoolSize());
+            System.out.println("Queue Size: " + executorService.getQueue().size());
+            System.out.println("===========================");
+        }, 0, 2, TimeUnit.SECONDS);
+
+        try {
+            List<Future<String>> futures = executorService.invokeAll(tasks);
+
+            for (Future<String> future : futures) {
+                System.out.println(future.get());
+            }
+
+            long end = System.currentTimeMillis();
+
+            System.out.println("Out Cost time: " + (end - start) + "ms");
+            System.out.println("Out Cost size: " + map.size());
+            System.out.println("Average generate " + map.size() / (end - start) + " id per ms");
+            System.out.println("Average Spin time for each id: " + spin.get()
+                    .divide(BigDecimal.valueOf(map.size()), 2, RoundingMode.HALF_UP));
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        } finally {
+            executorService.close();
+        }
+    }
 
 }
